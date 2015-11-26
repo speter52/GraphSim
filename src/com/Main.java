98dd7814a47881f0c59f4d9324aa074c0da7c8e9
Simon Peter <speter3@illinois.edu>
2015-11-25 16:54:52 -0800

    Added error message if input graph isn't specified.

--- src/com/Main.java
+++ src/com/Main.java
@@ -6,7 +6,14 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException
     {
-        Launcher.launchNetwork(args[0]);
+        if(args.length == 1)
+        {
+            Launcher.launchNetwork(args[0]);
+        }
+        else
+        {
+            System.out.println("Need input YAML file that represents the nodes in the network.");
+        }
 
         System.exit(0);
     }
