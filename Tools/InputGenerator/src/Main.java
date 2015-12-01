60a3f9f55317e7520adc1476b1302cddc37f69a4
Simon Peter <speter3@illinois.edu>
2015-12-01 13:52:11 -0600

    Fixed bug in Input Generator command parsing

--- Tools/InputGenerator/src/Main.java
+++ Tools/InputGenerator/src/Main.java
@@ -6,11 +6,11 @@ public class Main
     {
         int numberOfNodes = Integer.parseInt(args[0]);
 
-        int numberOfNeighbors = Integer.parseInt(args[1]);
+        int numberOfPartitions = Integer.parseInt(args[1]);
 
-        int numberOfPartitions = Integer.parseInt(args[2]);
+        int numberOfNeighbors = Integer.parseInt(args[2]);
 
-        String[] stateVariables = Arrays.copyOfRange(args, 2, args.length);
+        String[] stateVariables = Arrays.copyOfRange(args, 3, args.length);
 
         InputGenerator generator = new InputGenerator(numberOfNodes, numberOfNeighbors,
                                                         numberOfPartitions, stateVariables);
